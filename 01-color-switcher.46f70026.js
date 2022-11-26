e7d152a96dc72e5ae70256d8239741eb33dfc75d
Dmitry-Abakumov <108060418+Dmitry-Abakumov@users.noreply.github.com>
2022-11-26 21:26:03 +0000

    Deploying to gh-pages from @ Dmitry-Abakumov/goit-js-hw-09@f62add8e2d95e901247146614c2c908ff680ca2d 🚀

---
+++ 01-color-switcher.46f70026.js
@@ -0,0 +1,2 @@
+let t=null;const e={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};e.startBtn.addEventListener("click",(r=>{r.currentTarget.setAttribute("disabled",""),e.stopBtn.removeAttribute("disabled"),t=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.stopBtn.addEventListener("click",(r=>{r.currentTarget.setAttribute("disabled",""),e.startBtn.removeAttribute("disabled"),clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.46f70026.js.map
